ebaac2b9f8062c026eec9096e2731e4d584d821e
Борислав Христов <hristovborislav45@gmail.com>
2024-10-16 14:33:05 +0300

    Load dev.bg and get number of job offers

--- main.py
+++ main.py
@@ -12,33 +12,54 @@ from selenium.webdriver.support import expected_conditions as EC
 service = Service(executable_path='./driver/geckodriver.exe')
 driver = webdriver.Firefox(service=service)
 driver.set_window_size(700, 700)
+driver.implicitly_wait(3)
 
-with open('technologies.json', 'r') as file:
+def main():
+  init()
+  load()
+  n = get_job_offers_count()
+  try:
+    for i in range(n):
+      jobs = driver.find_elements(By.CLASS_NAME)
+      tech_usage = extract_technologies(jobs, technologies)
+      for tech, count in tech_usage.most_common():
+        print(f'{tech}: {count}')
+  except Exception as e:
+    print(f"Error: {e}")
+  finally:
+    driver.quit()
+
+def init():
+  global technologies
+  global job_descriptions
+  with open('technologies.json', 'r') as file:
     technologies = json.load(file)
-
-with open('job_descriptions.json', 'r') as file:
+  with open('job_descriptions.json', 'r') as file:
     job_descriptions = json.load(file)
 
+def get_job_offers_count():
+  total_jobs_div = WebDriverWait(driver, 1).until(
+     EC.visibility_of_element_located((By.CLASS_NAME, "facetwp-facet-total_items"))
+  )
+  total_jobs_text = total_jobs_div.text
+  total_jobs_count = int(total_jobs_text.split()[0])
+  return total_jobs_count
 
 def extract_technologies(jobs, tech_list):
-    tech_counter = Counter()
-    for job in jobs:
-        job_cleaned = re.sub(r'[^\w\s]', '', job).lower()
-        for tech in tech_list:
-            match, score = process.extractOne(tech.lower(), [job_cleaned])
-            if score > 80: 
-                tech_counter[tech] += 1
-    return tech_counter
-
-tech_usage = extract_technologies(job_descriptions, technologies)
+  tech_counter = Counter()
+  for job in jobs:
+    job_cleaned = re.sub(r'[^\w\s]', '', job).lower()
+    for tech in tech_list:
+      score = process.extractOne(tech.lower(), [job_cleaned])
+      if score > 80: 
+        tech_counter[tech] += 1
+  return tech_counter
 
 def load():
-  driver.get("https://www.google.com/search?q=tic+tac+toe")
-  WebDriverWait(driver, 1).until(
-    lambda d: d.execute_script("return document.readyState") == "complete"
-  )
-  div = driver.find_element(By.CSS_SELECTOR, "div.QS5gu.sy4vM")
-  div.click()
+  driver.get("https://dev.bg/company/jobs/back-end-development/?_job_location=remote") # itjobs will ban you
+  accept = driver.find_element(By.CLASS_NAME, "cmplz-accept")
+  accept.click()
   time.sleep(2)
-for tech, count in tech_usage.most_common():
-    print(f'{tech} {count}')
+
+if __name__ == "__main__":
+  main()

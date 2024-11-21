b15eaad3969f913e9ed39056ae826eacb0d2ef8d
Борислав Христов <hristovborislav45@gmail.com>
2024-11-21 20:16:36 +0200

    Add filter groups and styles

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QVBoxLayout, QPushButton, QProgressBar, QCheckBox, QWidget, QLabel, QComboBox
+    QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QProgressBar, QCheckBox, QWidget, QLabel, QComboBox, QGroupBox
 )
 from PySide6.QtCore import Qt, QThread, Signal
 import time
@@ -24,7 +24,10 @@ class AppWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Job Scraper")
-        self.setGeometry(100, 100, 600, 400)
+        self.setGeometry(100, 100, 700, 500)
+
+        with open("styles.css", "r") as file:
+            self.setStyleSheet(file.read())
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
@@ -47,39 +50,10 @@ class AppWindow(QMainWindow):
         self.category_selector.currentIndexChanged.connect(self.update_filters)
         self.layout.addWidget(self.category_selector)
 
-        self.layout.addWidget(QLabel("Filters:"))
-        self.filters = {}
-        self.filter_options = {
-            "Fully Remote": "_job_location=remote",
-            "Sofia": "_job_location=sofiya",
-            "Intern/Junior": "_seniority=intern",
-            "1-2 Years Experience": "_seniority=junior",
-            "2-5 Years Experience": "_seniority=mid-level",
-            "5+ Years Experience": "_seniority=senior",
-            "Team Lead": "_seniority=grand-master",
-            "With Mentioned Salary": "_salary=1",
-            "Small Company": "_it_employees_count=35%2C36",
-            "Big Company": "_it_employees_count=37%2C38"
-        }
-
-        for filter_name in self.filter_options:
-            checkbox = QCheckBox(filter_name)
-            self.filters[filter_name] = checkbox
-            self.layout.addWidget(checkbox)
-
-        self.additional_filters = {
-            "TypeScript": "_ts_front_end_development=typescript",
-            "Node.js": "_ts_front_end_development=node-js",
-            "Redux": "_ts_front_end_development=redux",
-            "Express": "_ts_front_end_development=express",
-            "Next.js": "_ts_front_end_development=next-js"
-        }
-
-        self.front_end_options = {}
-        for filter_name in self.additional_filters:
-            checkbox = QCheckBox(filter_name)
-            self.front_end_options[filter_name] = checkbox
-            self.layout.addWidget(checkbox)
+        # Filters grouped into categories
+        self.filters_layout = QVBoxLayout()
+        self.create_filter_groups()
+        self.layout.addLayout(self.filters_layout)
 
         self.scrape_button = QPushButton("Scrape All Jobs")
         self.scrape_button.clicked.connect(self.start_scraping)
@@ -98,35 +72,93 @@ class AppWindow(QMainWindow):
         self.scrape_thread = None
         self.update_filters()
 
+    def create_filter_groups(self):
+        self.filter_options = {
+            "Location": {
+                "Fully Remote": "_job_location=remote",
+                "Sofia": "_job_location=sofiya"
+            },
+            "Seniority": {
+                "Intern/Junior": "_seniority=intern",
+                "1-2 Years Experience": "_seniority=junior",
+                "2-5 Years Experience": "_seniority=mid-level",
+                "5+ Years Experience": "_seniority=senior",
+                "Team Lead": "_seniority=grand-master"
+            },
+            "Team Size": {
+                "Small Company": "_it_employees_count=35%2C36",
+                "Big Company": "_it_employees_count=37%2C38"
+            },
+            "Salary": {
+                "With Mentioned Salary": "_salary=1"
+            },
+            "Tech Stack": {
+                "TypeScript": "_ts_front_end_development=typescript",
+                "Node.js": "_ts_front_end_development=node-js",
+                "Redux": "_ts_front_end_development=redux",
+                "Express": "_ts_front_end_development=express",
+                "Next.js": "_ts_front_end_development=next-js"
+            }
+        }
+        self.filter_checkboxes = {}
+
+        for category, options in self.filter_options.items():
+            group_box = QGroupBox(category)
+            group_layout = QVBoxLayout()
+            for filter_name, param in options.items():
+                checkbox = QCheckBox(filter_name)
+                self.filter_checkboxes[filter_name] = checkbox
+                group_layout.addWidget(checkbox)
+            group_box.setLayout(group_layout)
+            self.filters_layout.addWidget(group_box)
+
     def update_filters(self):
         category = self.category_selector.currentText()
         if category == "ERP/CRM Development":
-            self.filters["With Mentioned Salary"].setVisible(False)
+            self.filter_checkboxes["With Mentioned Salary"].setVisible(False)
         else:
-            self.filters["With Mentioned Salary"].setVisible(True)
+            self.filter_checkboxes["With Mentioned Salary"].setVisible(True)
 
         is_front_end = category == "Front-End Development"
-        for checkbox in self.front_end_options.values():
+        tech_stack_group = self.find_group_box("Tech Stack")
+        for checkbox in tech_stack_group.findChildren(QCheckBox):
             checkbox.setVisible(is_front_end)
+
+    def find_group_box(self, name):
+        for i in range(self.filters_layout.count()):
+            group_box = self.filters_layout.itemAt(i).widget()
+            if isinstance(group_box, QGroupBox) and group_box.title() == name:
+                return group_box
+        return None
 
     def get_selected_filters(self):
         base_url = self.categories[self.category_selector.currentText()]
+        location_params = []
+        seniority_params = []
         query_params = []
 
-        for filter_name, checkbox in self.filters.items():
+        for filter_name, checkbox in self.filter_checkboxes.items():
             if checkbox.isChecked():
-                param = self.filter_options.get(filter_name, "")
-                query_params.append(param)
+                param = self.get_filter_param(filter_name)
+                if "job_location" in param:
+                    location_params.append(param.split("=")[1])
+                elif "seniority" in param:
+                    seniority_params.append(param.split("=")[1])
+                else:
+                    query_params.append(param)
 
-        if self.category_selector.currentText() == "Front-End Development":
-            additional_params = []
-            for filter_name, checkbox in self.front_end_options.items():
-                if checkbox.isChecked():
-                    additional_params.append(self.additional_filters[filter_name])
-            if additional_params:
-                query_params.append("_ts_front_end_development=" + "%2C".join(additional_params))
+        if location_params:
+            query_params.append(f"_job_location={'%2C'.join(location_params)}")
+        if seniority_params:
+            query_params.append(f"_seniority={'%2C'.join(seniority_params)}")
 
         return base_url + "?" + "&".join(query_params)
+
+    def get_filter_param(self, filter_name):
+        for category, options in self.filter_options.items():
+            if filter_name in options:
+                return options[filter_name]
+        return ""
 
     def start_scraping(self):
         selected_filters = self.get_selected_filters()
